7039c719a2b677bb531bd5a3607f3895f7e4fc8b
kudomat0 <zekilatiefpro@gmail.com>
2024-02-26 13:46:39 +0700

    update

--- node_modules/eldechat/chat/Afrika/index.js
+++
@@ -1,7 +0,0 @@
-const cowsay = require('cowsay');
-
-function haloDuniaArmadillo() {
-  console.log(cowsay.say({ text: "hallo wêrelde", f: "armadillo" }));
-}
-
-module.exports = haloDuniaArmadillo;
